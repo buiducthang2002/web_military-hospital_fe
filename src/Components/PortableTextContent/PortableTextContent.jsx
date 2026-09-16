f9b80c5b9897681a558dc1462cce078c3b304622
Bui Duc Thang <buiducthang2002@gmail.com>
2026-09-16 16:02:28 +0700

    Revert "Fix: use urlFor instead of getImageUrl for image rendering"
    This reverts commit cf90d8b12a33a230dcf28f13f33d24217aef3599.

--- src/Components/PortableTextContent/PortableTextContent.jsx
+++ src/Components/PortableTextContent/PortableTextContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PortableText } from '@portabletext/react'
-import { urlFor } from '../../lib/sanity'
+import { getImageUrl } from '../../lib/sanity'
 
 const PortableTextContent = ({ content }) => {
   if (!content) {
@@ -16,7 +16,7 @@ const PortableTextContent = ({ content }) => {
   const components = {
     types: {
       image: ({ value }) => {
-        const imageUrl = urlFor(value.asset).url()
+        const imageUrl = getImageUrl(value.asset._ref).url()
         return (
           <figure className="portable-text-image">
             <img
